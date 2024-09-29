39572fde14e8822ffd2a22d58b05f9fb5bb2014e
tksarah <tsarah8139@gmail.com>
2024-09-30 01:10:57 +0900

    update download.sh

--- download.sh
+++ download.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
+# Clone files
+git clone https://github.com/Soneium/soneium-node.git
+
+cd soneium-node/minato || exit
+
 # Generate jwt.txt
 openssl rand -hex 32 > jwt.txt
-
-# Download and rename files
-declare -A files=(
-    ["minato-genesis.json"]="https://docs.soneium.org/assets/files/minato-genesis-5e5db79442a6436778e9c3c80a9fd80d.json"
-    ["docker-compose.yml"]="https://docs.soneium.org/assets/files/docker-compose-003749bd470bb0677fb5b8e2a82103ed.yml"
-    ["minato-rollup.json"]="https://docs.soneium.org/assets/files/minato-rollup-6d00cc672bf6c8e9c14e3244e36a2790.json"
-    ["sample.env"]="https://docs.soneium.org/assets/files/sample-4ab2cad1f36b3166b45ce4d8fed821ab.env"
-)
-
-for file in "${!files[@]}"; do
-    wget -q "${files[$file]}" -O "${file}"
-done
 
 # Rename sample.env
 cp sample.env .env
