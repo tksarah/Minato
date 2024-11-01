190c2c096f1c93cca895e8033de10919992cdc5a
tksarah <tsarah8139@gmail.com>
2024-11-01 17:59:49 +0900

    Add loading .env

--- snippet/checkblock.sh
+++ snippet/checkblock.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cast block-number --rpc-url http://<your pub ip address>:8545
+
+# Load Environment Variables
+if [ -f .env ]; then
+  export $(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
+fi
+
+cast block-number --rpc-url http://$P2P_ADVERTISE_IP:8545
