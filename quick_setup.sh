598fb39017514f3b41894b3decac2e6fdf5bf9da
tksarah <tsarah8139@gmail.com>
2024-12-07 08:41:26 +0900

    update -y

--- quick_setup.sh
+++ quick_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### Setup OS
-apt-get update
+apt-get update -y
 apt-get install ca-certificates curl -y
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
